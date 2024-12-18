1a173e94c14bd323fceb1c5c7e3dae37f830e17c
germansosagoy <germanesosagoy@gmail.com>
2024-12-18 18:52:34 -0300

    chore(cart): refactor quantity and pricing logic

--- app/components/Cart/CartSidebar.tsx
+++ app/components/Cart/CartSidebar.tsx
@@ -3,6 +3,7 @@ import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import { formatPrice } from "@/app/lib/formatPrice";
+import QuantitySelector from "../ui/quantity-selector";
 
 interface CartSidebarProps {
   isOpen: boolean;
@@ -11,12 +12,25 @@ interface CartSidebarProps {
 
 
 export default function CartSidebar({isOpen, onClose}: CartSidebarProps) {
-  const { cart, removeFromCart } = useCart();
-
+  const { cart, updateQuantity, removeFromCart} = useCart();
+  
+  if (!isOpen) return null;
+  
   const totalAmount = cart.items.reduce((total, item) => {
-    return total + (item.product.price * item.quantity); // multiplicar el precio por la cantidad del producto
+    return total + item.product.price * item.quantity;
   }, 0);
 
+  // const totalAmount = cart.items.reduce((total, item) => {
+  //   const unitValue = item.product.unitValue || 1; 
+  
+  //   const itemQuantity = item.product.salesUnit === 'group'
+  //     ? item.quantity * unitValue // si el producto'group', usa la cantidad convertida a unidades
+  //     : item.quantity; // para otros casos de productos
+    
+  //   return total + item.product.price * itemQuantity;
+  // }, 0);
+  
+  
   return (
     <>
     {isOpen && (
@@ -26,7 +40,7 @@ export default function CartSidebar({isOpen, onClose}: CartSidebarProps) {
       />
     )}
     <div
-      className={`fixed top-0 right-0 h-screen w-full sm:w-[440px] overflow-auto border-l border-gray-400 bg-white shadow-xl transform ${
+      className={`fixed top-0 right-0 h-screen w-full sm:w-[480px] overflow-auto border-l border-gray-400 bg-white shadow-xl transform ${
         isOpen ? "translate-x-0" : "translate-x-full"
       } transition-transform duration-300 ease-in-out px-4 py-10 sm:rounded-l-md z-50`}
     >
@@ -39,26 +53,29 @@ export default function CartSidebar({isOpen, onClose}: CartSidebarProps) {
       <div className="flex-1 overflow-y-auto p-4 space-y-4 border-t">
         {cart.items.length > 0 ? (
           cart.items.map((item) => (
-            <div
-              key={item.product.id}
-              className="flex items-center justify-between border-b py-4"
-            >
-              {/* Producto */}
-              <div className="flex items-center space-x-4">
-                <Image
-                  src={item.product?.image ?? ""}
-                  priority
-                  width={80}
-                  height={80}
-                  alt={item.product.title}
-                  className="object-contain rounded-lg"
+            <div key={item.product.id} className="flex items-center justify-between border-b py-4">
+            {/* producto en el carrito */}
+            <div className="flex items-center space-x-4">
+              <Image
+                src={item.product?.image ?? ""}
+                priority
+                width={80}
+                height={80}
+                alt={item.product.title}
+                className="object-contain rounded-lg"
+              />
+              <div className="flex flex-col space-y-4">
+                <h3 className="font-medium text-base line-clamp-3">
+                  {item.product?.title || "Producto desconocido"}
+                </h3>
+                <QuantitySelector
+                  product={item.product}
+                  value={item.quantity} // cantidad actual del producto en el carrito
+                  onQuantityChange={(newQuantity) => updateQuantity(item.product.id, newQuantity)}
+                  context="cart"
                 />
-                <div>
-                  <h3 className="font-medium text-base">
-                    {item.product?.title || "Producto desconocido"}
-                  </h3>
-                </div>
               </div>
+            </div>
               <button onClick={() => removeFromCart(item.product.id)} className="text-red-500 text-sm px-2 py-1 border rounded hover:text-red-700 hover:border-red-700">
                 Quitar
               </button>
--- app/components/ProductCard/ProductCardDetails.tsx
+++ app/components/ProductCard/ProductCardDetails.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Product } from "@/app/types/product-types";
 // import { calculateDiscount } from "@/app/lib/calculateDiscount";
 import { formatPrice } from "@/app/lib/formatPrice";
@@ -20,14 +20,26 @@ const ProductCardDetails: React.FC<ProductCardDetailsProps> = ({ product }) => {
   const cartItem = cart.items.find((item) => item.product.id === product.id);
   const currentQuantity = cartItem ? cartItem.quantity : 0;
 
-  // Función para manejar el cambio de cantidad
+  const [selectedQuantity, setSelectedQuantity] = useState<number>(currentQuantity);
+
+  // actualizar la cantidad seleccionada 
   const handleQuantityChange = (quantity: number) => {
-    if (quantity === 0) {
-      removeFromCart(product.id); // Si la cantidad es 0, eliminamos el producto
-    } else {
-      addToCart(product, quantity); // Si no, agregamos o actualizamos el producto
+    setSelectedQuantity(quantity);
+  };
+
+  // agregar o actualizar un producto en el carrito
+  const handleAddToCart = () => {
+    if (selectedQuantity > 0) {
+      addToCart(product, selectedQuantity);
     }
   };
+
+   // eliminar un producto del carrito
+   const handleRemoveFromCart = () => {
+    removeFromCart(product.id);
+    setSelectedQuantity(0); 
+  };
+
 
   return (
     <div className="flex flex-col sm:flex-row items-center justify-between bg-white p-6 py-2 sm:py-14 sm:px-5 rounded-lg shadow w-full max-w-4xl mx-auto">
@@ -63,34 +75,36 @@ const ProductCardDetails: React.FC<ProductCardDetailsProps> = ({ product }) => {
         )}
       </div>
       <span className="text-xl font-bold text-gray-800 mb-2">{formatPrice(product.price)}</span>
-      
-      {/* Selector de cantidad */}
-      <div className="my-6">
-        <QuantitySelector product={product} value={currentQuantity} onQuantityChange={handleQuantityChange} />
-      </div>
+      {/* selector de cantidad */}
+        <div className="my-6">
+         <QuantitySelector
+          product={product}
+          value={selectedQuantity}
+          onQuantityChange={handleQuantityChange}
+          data-testid={`quantity-${product.salesUnit}-input`}
+          />
+        </div>
       <p className="text-gray-400 line-clamp-1 mb-4">{product.description}</p>
 
-      {/* Botones de acción */}
-      <div className="flex flex-col mt-10 space-y-4">
-        <button className="bg-[#264b97] text-white px-5 py-3 rounded-full border-[#254a96] text-md font-semibold text-[#264b97] hover:bg-black transition duration-200">
-          Comprar ahora
-        </button>
-        {currentQuantity === 0 ? (
-          <button
-            onClick={() => addToCart(product, 1)}
-            className="flex justify-center items-center bg-white text-[#264b97] px-5 py-3 border-2 border-[#254a96] rounded-full text-md font-semibold transition-all hover:bg-black hover:text-white"
+      {/* botones */}
+      <div className="flex flex-col mt-2">
+        <div className="flex flex-col mt-10 space-y-4">
+          <button onClick={currentQuantity === 0 ? handleAddToCart : handleRemoveFromCart} className={`flex justify-center items-center bg-white text-[#264b97] px-5 py-3 border-2 border-[#254a96] rounded-full text-md font-semibold transition-all ${
+              currentQuantity === 0
+                ? "hover:bg-black hover:text-white"
+                : "hover:bg-red-600 hover:border-red-600 hover:text-white"
+            }`}
           >
-            Agregar
-            <ShoppingCart className="ml-2" size={20} />
+            {currentQuantity === 0 ? (
+              <>
+                Agregar
+                <ShoppingCart className="ml-2" size={20} />
+              </>
+            ) : (
+              "Eliminar producto"
+            )}
           </button>
-        ) : (
-          <button
-            onClick={() => removeFromCart(product.id)}
-            className="flex justify-center items-center bg-white text-[#264b97] px-5 py-3 border-2 border-[#254a96] rounded-full text-md font-semibold transition-all hover:bg-red-600 hover:border-red-600 hover:text-white"
-          >
-            Eliminar del carrito
-          </button>
-        )}
+        </div>
       </div>
     </div>
   </div>
